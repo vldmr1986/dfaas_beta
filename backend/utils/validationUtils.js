3e9c5da137a833fa0fc9487d6d2ab248f76dec9e
vb <vldmr1986@gmail.com>
2022-11-29 12:37:44 +0100

    Backend: remove space_name logic

--- backend/utils/validationUtils.js
+++ backend/utils/validationUtils.js
@@ -2,7 +2,7 @@ const validators = [
     {field: "name", validator: ({name})=> name && name.trim().length ? null : "Invalid Name"},
     {field: "surname", validator: ({surname})=> surname && surname.trim().length ? null : "Invalid surname"},
     {field: "email", validator: ({email})=> email && email.trim().length ? null : "Invalid email"},
-    {field: "space", validator: ({space})=> space ? null : "No available spacename"},
+    // {field: "space", validator: ({space})=> space ? null : "No available spacename"},
     {field: "country", validator: ({country})=> country ? null : "Invalid country"},
     {field: "isEmailInUse", validator: ({isEmailInUse})=> !isEmailInUse ? null : "This user is already invited for Data Fabric beta access."},
 ];
