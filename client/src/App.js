c6e75a9d98e126c2ea7bc69771ae9e623178d1c8
vb <vldmr1986@gmail.com>
2023-01-17 13:54:07 +0100

    G-analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,49 @@
 import React from "react";
-import { DfHeaderbar } from "./components";
+import ReactGA from 'react-ga';
+import {DfHeaderbar} from "./components";
 // import SignUp from "./components/signupForm";
-import { SignupPage, SignupSuccessPage, SignupWarningPage } from "./pages";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import {SignupPage, SignupSuccessPage, SignupWarningPage} from "./pages";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+import {WithAnalytics} from "./withAnalytics";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: true,
-      staleTime: 60 * 1000,
-      cacheTime: 60 * 1000,
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: true, staleTime: 60 * 1000, cacheTime: 60 * 1000,
+        },
     },
-  },
 });
 
+ReactGA.initialize('G-YTC2BR4296');
+
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignupPage />} />
-          {/* <Route path="/" element={<SignUp />} /> */}
-          <Route
-            path="/success"
-            element={
-              <>
-                <DfHeaderbar />
-                <SignupSuccessPage />
-              </>
-            }
-          />
-          <Route
-            path="/error"
-            element={
-              <>
-                <DfHeaderbar />
-                <SignupWarningPage />
-              </>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-    </QueryClientProvider>
-  );
+    return (<QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<WithAnalytics>
+                        <SignupPage/>
+                    </WithAnalytics>}/>
+                    {/* <Route path="/" element={<SignUp />} /> */}
+                    <Route
+                        path="/success"
+                        element={<WithAnalytics>
+                            <DfHeaderbar/>
+                            <SignupSuccessPage/>
+                        </WithAnalytics>}
+                    />
+                    <Route
+                        path="/error"
+                        element={<WithAnalytics>
+                            <DfHeaderbar/>
+                            <SignupWarningPage/>
+                        </WithAnalytics>}
+                    />
+                </Routes>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
+        </QueryClientProvider>);
 }
 
 export default App;
